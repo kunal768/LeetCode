b6731a182aca172d0d7735101837dc4ad3291479
kunal768 <33108756+kunal768@users.noreply.github.com>
2020-05-01 20:41:29 +0530

    Create firstBadVersion.py

---
+++ Challenges/May/Week1/firstBadVersion.py
@@ -0,0 +1,20 @@
+class Solution:
+    def firstBadVersion(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        start = 1
+        end = n
+        while start <= end:
+            mid = (start + end) // 2
+            if isBadVersion(mid):
+                if not isBadVersion(mid -1):
+                    return mid;
+                else:
+                    end = mid;
+            else:
+                if isBadVersion(mid + 1):
+                    return mid + 1        
+                else:
+                    start = mid + 1;
