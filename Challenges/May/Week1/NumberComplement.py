51b1b607c0a1a8cd8894db452cad7825bf128afe
kunal768 <33108756+kunal768@users.noreply.github.com>
2020-05-04 13:49:19 +0530

    Update NumberComplement.py

--- Challenges/May/Week1/NumberComplement.py
+++ Challenges/May/Week1/NumberComplement.py
@@ -1,3 +1,14 @@
+# good way XORING with an array of 1's
+class Solution:
+    def bitwiseComplement(self, N: int) -> int:
+        if N == 0 : return 1
+        if N == 1  : return 0
+        c = 1
+        while c <= N:
+            c <<= 1
+        return (c-1) ^ N
+
+
 # noob way (Doing it just coz i can :p)
 class Solution:
     def findComplement(self, num: int) -> int:
