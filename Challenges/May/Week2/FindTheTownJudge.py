0dfb4079f0db29bb0f4aca6f79dc201ccc8acff5
kunal768 <33108756+kunal768@users.noreply.github.com>
2020-05-10 13:24:24 +0530

    Update FindTheTownJudge.py

--- Challenges/May/Week2/FindTheTownJudge.py
+++ Challenges/May/Week2/FindTheTownJudge.py
@@ -24,4 +24,36 @@ class Solution:
             return -1
         else:
             return judge
+  
+# and then .... when i saw other solutions and came to know my solution was pretty stupid ... i realised this is a good method 
+# of learning so without further ado .... 
+
+# O(n) method and i didnt realise this is a Graph Concept
+# Considet "trust" as a directed Graph -> then degree is the incoming and outgoing edges 
+# if degree of a vertex == N-1 means its trusted by every other vertex making it the judge 
+class Solution:
+    def findJudge(self, N: int, trust: List[List[int]]) -> int:
+        # corner case 
+        if len(trust) == 0:
+            if N > 1 :
+                return -1
+            return 1
+        
+        degrees = [0]*(N+1)
+        for (i,j) in trust :
+            # they trust someone so outward edge
+            degrees[i] -= 1
+            # they are being trusted so inwards edge
+            degrees[j] += 1
+    
+        for i in range(len(degrees)) :
+            # if some vertex has degree N - 1 its the judge
+            if degrees[i] == N - 1 :
+                return i 
+        return -1
                 
+
+
+
+
+
