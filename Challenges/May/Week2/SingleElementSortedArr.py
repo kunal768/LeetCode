5eba6ac742f11a53a4877bae5c0d2da07cae3824
kunal768 <33108756+kunal768@users.noreply.github.com>
2020-05-12 13:27:57 +0530

    Update SingleElementSortedArr.py

--- Challenges/May/Week2/SingleElementSortedArr.py
+++ Challenges/May/Week2/SingleElementSortedArr.py
@@ -1,3 +1,18 @@
+# Binary Search O(logn) runtime and O(1) space
+class Solution:
+    def singleNonDuplicate(self, nums: List[int]) -> int:
+        lo, hi = 0, len(nums) - 1
+        while lo < hi:
+            m = (lo + hi) // 2
+            if m % 2 == 1 and nums[m - 1] == nums[m]:
+                lo = m + 1
+            elif  m % 2 == 0 and nums[m + 1] == nums[m]:
+                lo = m + 2
+            else:
+                hi = m
+        return nums[lo]
+    
+
 # O(n) runtime and O(1) space
 class Solution:
     def singleNonDuplicate(self, nums: List[int]) -> int:
