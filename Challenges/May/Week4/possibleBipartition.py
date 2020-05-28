f750861900b40c1cbb3974906162b4d6d5b32241
kunal768 <33108756+kunal768@users.noreply.github.com>
2020-05-28 15:41:50 +0530

    Update possibleBipartition.py

--- Challenges/May/Week4/possibleBipartition.py
+++ Challenges/May/Week4/possibleBipartition.py
@@ -1,3 +1,33 @@
+# this is understandable 
+
+class Solution:
+    def possibleBipartition(self, N: int, dislikes: List[List[int]]) -> bool:
+        graph = collections.defaultdict(lambda : [])
+        for u,v in dislikes :
+            graph[u].append(v)
+            graph[v].append(u)
+        
+        color = dict()
+        def dfs(node):
+            for adj in graph[node]:
+                if adj in color:
+                    if color[adj] == color[node]:
+                        return False
+                else:
+                    color[adj] = 1 - color[node]
+                    if not dfs(adj):
+                        return False
+            return True
+        for node in graph:
+            if node not in color :
+                color[node] = 0 
+                if not dfs(node):
+                    return False
+        return True
+        
+
+
+
 # check if graph is bipartite
 class Solution:
     def possibleBipartition(self, N: int, dislikes: List[List[int]]) -> bool:
