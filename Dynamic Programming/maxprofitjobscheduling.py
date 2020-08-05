07e42d954805b736c2ae8cb732a35bd26fe7f60c
kunal768 <33108756+kunal768@users.noreply.github.com>
2020-08-05 17:36:37 +0530

    Create maxprofitjobscheduling.py

---
+++ Dynamic Programming/maxprofitjobscheduling.py
@@ -0,0 +1,23 @@
+
+class Solution:
+    def jobScheduling(self, startTime: List[int], endTime: List[int], profit: List[int]) -> int:
+        n = len(startTime)
+        jobs = [(startTime[i],endTime[i],profit[i]) for i in range(n)]
+        jobs.sort(key = lambda x : x[1])
+        dp = [0]*n
+        dp[0] = jobs[0][2]
+        def lastNonConflict(i):
+            for j in range(i-1,-1,-1):
+                if jobs[j][1] <= jobs[i][0] :
+                    return j 
+            return -1
+        for i in range(1,n):
+            incl = jobs[i][2]
+            l = lastNonConflict(i)
+            if l != github-1 :
+                incl += dp[l] 
+            dp[i] = max(dp[i-1],incl)
+       
+        return dp[-1]
+    
+        
