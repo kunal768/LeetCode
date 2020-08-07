e830fa759f8c1dc130fbbfb84d92ce7a217378de
kunal768 <33108756+kunal768@users.noreply.github.com>
2020-08-07 13:38:02 +0530

    Create AirplaneSeatAssignment.py

---
+++ Dynamic Programming/AirplaneSeatAssignment.py
@@ -0,0 +1,10 @@
+class Solution:
+    def nthPersonGetsNthSeat(self, n: int) -> float:
+        if n == 0 :
+            return 0 
+        if n == 1 :
+            return 1
+        if n >= 2:
+            return 0.50000
+    
+        
