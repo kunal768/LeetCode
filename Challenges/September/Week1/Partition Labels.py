9ac683e3e2adc8a3e3429d81ce67bf5e248340a7
kunal768 <33108756+kunal768@users.noreply.github.com>
2020-09-13 16:45:24 +0530

    Update Partition Labels.py

--- Challenges/September/Week1/Partition Labels.py
+++ Challenges/September/Week1/Partition Labels.py
@@ -10,3 +10,21 @@ class Solution:
                 anchor = i+1
         return ans
         
+class Solution:
+    def partitionLabels(self, S: str) -> List[int]:
+        ans = []
+        if len(S) == 0 :
+            return ans
+        seen = set()
+        hm = collections.Counter(S)
+        count = 0 
+        for c in S :
+            count += 1
+            seen.add(c)
+            hm[c] -= 1
+            if hm[c] == 0:
+                seen.remove(c)
+                if not seen :
+                    ans.append(count)
+                    count = 0 
+        return ans
