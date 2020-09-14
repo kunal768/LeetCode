08a86f41b994ceafbaac74572c674410309e6654
kunal768 <33108756+kunal768@users.noreply.github.com>
2020-09-14 14:51:24 +0530

    Update House Robber.py

--- Challenges/September/Week2/House Robber.py
+++ Challenges/September/Week2/House Robber.py
@@ -1,3 +1,4 @@
+# Simple Recursive Approach
 from functools import lru_cache
 class Solution:
     def rob(self, nums: List[int]) -> int:
@@ -9,3 +10,19 @@ class Solution:
             return max((nums[i]+dfs(i+2) if i < len(nums) else 0) ,(nums[i+1]+dfs(i+3) if i + 1 < len(nums) else 0 ))
         
         return dfs(0)
+    
+# Table Approach 
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        if not nums :
+            return 0
+        n = len(nums)
+        if n == 1 : 
+            return nums[0]
+        dp = [0]*n
+        dp[0],dp[1] = nums[0],max(nums[0],nums[1])
+        for i in range(2,n):
+            dp[i] = max(dp[i-1],dp[i-2]+nums[i])
+        # print(dp)
+        return dp[n-1]
+        
