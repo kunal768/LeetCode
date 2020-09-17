091f5dd066704e4ac96aa163e13a1e6b485c6c84
kunal768 <33108756+kunal768@users.noreply.github.com>
2020-09-17 16:21:07 +0530

    Create Maximum XOR of Two Numbers in an Array.py

---
+++ Challenges/September/Week3/Maximum XOR of Two Numbers in an Array.py
@@ -0,0 +1,18 @@
+class Solution:
+    def findMaximumXOR(self, arr: List[int]) -> int:
+        maxx,mask = 0,0
+        se = set()
+        for i in range(30,-1,-1):
+            mask |= 1 << i
+            newMaxx = maxx | (1 << i)
+        
+            for i in range(len(arr)):
+                se.add(arr[i] & mask)
+
+            for prefix in se :
+                if newMaxx ^ prefix in se :
+                    maxx = newMaxx
+                    break
+= 
+            se.clear()
+        return maxx
