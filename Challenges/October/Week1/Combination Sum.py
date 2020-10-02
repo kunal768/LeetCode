af9fb37a4515c18e77d8b4f77b8ba66ca61be971
kunal768 <33108756+kunal768@users.noreply.github.com>
2020-10-02 13:46:40 +0530

    Update Combination Sum.py

--- Challenges/October/Week1/Combination Sum.py
+++ Challenges/October/Week1/Combination Sum.py
@@ -14,3 +14,21 @@ class Solution:
             
         dfs(0, [])
         return ans
+
+# Another Way 
+class Solution:
+    def dfs(self,candidates,res,path,target,index,n):
+        if sum(path) == target or index >= n:
+            res.append(path)
+            return
+        if sum(path) > target :
+            return 
+    
+        for i in range(index,n):
+            self.dfs(candidates,res,path+[candidates[i]],target,i,n)
+        
+    def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
+        res = []
+        self.dfs(candidates,res,[],target,0,len(candidates))
+        return res
+        
